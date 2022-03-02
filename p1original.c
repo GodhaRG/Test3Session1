809538b60751b433a967a3d9b77e8ee53d83c7d9
GodhaRG <96509418+GodhaRG@users.noreply.github.com>
2022-03-02 04:45:06 +0000

    done 1 and 3

--- p1original.c
+++ p1original.c
@@ -1,2 +1,35 @@
-
+#include<stdio.h>
+void input(int*num1,int*den1,int*num2,int*den2)
+{
+ printf("enter the numerator and denominator of fraction 1\n");
+  scanf("%d %d",num1,den1);
+  printf("enter the numerator and denominator of fraction 2\n");
+  scanf(" %d  %d",num2,den2);
+}
+void add(int num1,int den1,int num2,int den2,int *num3,int *den3)
+{
+  if(den1==den2)
+  {
+    *num3=num1+num2;
+    *den3=den1;
+  }
+  else
+  {
+    *den3=den1*den2;
+    num1=num1*den2;
+    num2=num2*den1;
+    *num3=num1+num2;
+  }
+}
+void output(int num1,int den1,int num2,int den2,int num3,int den3)
+{
+  printf("the sum of %d / %d and %d / %d is %d /%d \n",num1,den1,num2,den2,num3,den3);
+}
+int main()
+{
+  int num1,den1,num2,den2,num3,den3;
+  input(&num1,&den1,&num2,&den2);
+  add(num1,den1,num2,den2,&num3,&den3);
+  output(num1,den1,num2,den2,num3,den3);
+  return 0;
 }
